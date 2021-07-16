2ff9714fcad1f2d6de9922fe64acbbf3aa012e8f
Amauri Lima <62773460+AmauriLima@users.noreply.github.com>
2021-07-16 16:32:56 -0300

    Removing redundancy

--- rocketQ/src/route.js
+++ rocketQ/src/route.js
@@ -1,9 +1,8 @@
 const { Router } = require('express');
-const express = require('express');
 const QuestionController = require('./controllers/QuestionController');
 const RoomController = require('./controllers/RoomController');
 
-const route = express.Router();
+const route = Router();
 
 route.get('/', (req, res) => res.render('index', {page: 'enter-room'}));
 route.get('/create-pass', (req, res) => res.render('index', {page: 'create-pass'}));
